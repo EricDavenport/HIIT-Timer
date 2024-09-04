dce22549ca7fd62d3dbb57894b6abb7903f6e7b8
Eric Davenport <Ericdavenport@pursuit.org>
2024-09-04 16:13:16 -0400

    Timer logic and main display

--- HIIT-Timer Watch App/ContentView.swift
+++ HIIT-Timer Watch App/ContentView.swift
@@ -1,12 +1,44 @@
 import SwiftUI
+import Combine
+import WatchKit
 
 struct ContentView: View {
+    @ObservedObject var workoutTimer = WorkoutTimer()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(workoutTimer.displayTime)
+                .font(.system(size: 40))
+                .padding()
+            
+            HStack {
+                // MARK: Start button
+                Button(action: {
+                    if workoutTimer.isActive {
+                        workoutTimer.pause()
+                    } else {
+                        do {
+                            try workoutTimer.start()
+                        } catch {
+                            print("Error starting timer: \(error.localizedDescription)")
+                        }
+                    }
+                }) {
+                    Image(systemName: workoutTimer.isActive ? "pause" : "play")
+                }
+                .padding()
+                .disabled(false)  // button always active
+                
+                // MARK: Stop Button
+                Button(action: {
+                    workoutTimer.stop()
+                }) {
+                    Image(systemName: "stop")
+                }
+                .padding()
+                .disabled(!workoutTimer.isPaused)  // only enabled when play == pause
+                
+            }
         }
         .padding()
     }
---
+++ HIIT-Timer Watch App/WorkoutTimer.swift
@@ -0,0 +1,81 @@
+import Foundation
+import Combine
+import WatchKit
+
+enum TimerError: Error {
+    case invalidInterval(String)
+}
+
+class WorkoutTimer: ObservableObject {
+    @Published var timeRemaining: TimeInterval = 0
+    @Published var isActive = false
+    @Published var isPaused = false
+    
+    // TODO: add a isFinished bool and do some animated celebration
+    // @Published var isFinished = false
+    
+    private var timer: Timer?
+    private var workDuration: TimeInterval = 30
+    private var restDuration: TimeInterval = 30
+    private var isResting: Bool = false
+    
+    var displayTime: String {
+        let minutes = Int(timeRemaining) / 60
+        let seconds = Int(timeRemaining) % 60
+        
+        return String(format:"%02d:%02d", minutes, seconds)
+    }
+    
+    func start() throws {
+        guard workDuration > 0, restDuration > 0 else {
+            throw TimerError
+                .invalidInterval("Work duration and rest duration must be greater than zero.")
+        }
+        
+        isActive = true
+        isPaused = false
+        isResting = false
+        timeRemaining = workDuration
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.updateTimer()
+        }
+    }
+    
+    func pause() {
+        guard isActive else { return }
+        isActive = false
+        isPaused = true
+        timer?.invalidate()
+    }
+    
+    func stop() {
+        guard isPaused else { return }
+        isActive = false
+        isPaused = false
+        timeRemaining = 0
+        timer?.invalidate()
+    }
+    
+    private func updateTimer() {
+        guard timeRemaining > 0 else {
+            if isResting {
+                timeRemaining = workDuration
+            } else {
+                timeRemaining = restDuration
+            }
+            isResting.toggle()
+            // MARK: Haptic feedback -> May need to adjust depending on real feel during workout
+            WKInterfaceDevice.current().play(.success)
+            return
+        }
+        timeRemaining -= 1
+    }
+    
+
+    
+    func setIntervals(work: TimeInterval, rest: TimeInterval) {
+        workDuration = work
+        restDuration = rest
+        timeRemaining = workDuration
+    }
+}
