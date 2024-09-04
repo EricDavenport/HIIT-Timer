437a65ae8745420104d31723d573b65299fbe03f
Eric Davenport <Ericdavenport@pursuit.org>
2024-09-04 16:57:11 -0400

    dismiss interval counter when opressing save button

--- HIIT-Timer Watch App/IntervalSettingsView.swift
+++ HIIT-Timer Watch App/IntervalSettingsView.swift
@@ -4,37 +4,42 @@ struct IntervalSettingsView: View {
     @State private var workDuration: Double = 30
     @State private var restDuration: Double = 30
     @ObservedObject var workoutTimer: WorkoutTimer
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            Text("Set Intervals")
-                .font(.headline)
-            
+        ScrollView {
             VStack {
-                Text("Work Duration:")
-                Slider(value: $workDuration, in: 10...300, step: 10)
-                Text("\(Int(workDuration))s")
-            }
-            .padding()
-            
-            VStack {
-                Text("Rest Duration:")
-                Slider(value: $restDuration, in: 10...300, step: 10)
-                Text("\(Int(restDuration))s")
-            }
-            .padding()
-            
-            Button("Save") {
-                do {
-                    try validateIntervals()
-                    workoutTimer.setIntervals(work: workDuration, rest: restDuration)
-                } catch {
-                    print("Invalid interval settings: \(error.localizedDescription).")
+                Text("Set Intervals")
+                    .font(.headline)
+                
+                VStack {
+                    Text("Work Duration:")
+                    Slider(value: $workDuration, in: 10...600, step: 10)
+                    Text("\(Int(workDuration))s")
                 }
+                .padding()
+                
+                VStack {
+                    Text("Rest Duration:")
+                    Slider(value: $restDuration, in: 10...600, step: 10)
+                    Text("\(Int(restDuration))s")
+                }
+                .padding()
+                
+                Button("Save") {
+                    do {
+                        try validateIntervals()
+                        workoutTimer.setIntervals(work: workDuration, rest: restDuration)
+                        
+                        presentationMode.wrappedValue.dismiss()
+                    } catch {
+                        print("Invalid interval settings: \(error.localizedDescription).")
+                    }
+                }
+                .padding()
             }
-            .padding()
+            .navigationTitle("Intervals")
         }
-        .navigationTitle("Intervals")
     }
     
     private func validateIntervals() throws {
